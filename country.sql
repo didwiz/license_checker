46f50a6fb87c307ee976f3c82048df005adc4010
idris <didwiz83@gmail.com>
2018-05-04 13:27:28 +0100

    updated dump files added

--- country.sql
+++ country.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 29, 2018 at 04:18 PM
+-- Generation Time: May 04, 2018 at 01:26 PM
 -- Server version: 5.6.16-1~exp1
 -- PHP Version: 7.0.28-0ubuntu0.16.04.1
 
@@ -20,20 +20,6 @@ SET time_zone = "+00:00";
 -- Database: `license_checker`
 --
 
--- --------------------------------------------------------
-
---
--- Table structure for table `country`
---
-
-CREATE TABLE `country` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `code` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 --
 -- Dumping data for table `country`
 --
@@ -41,25 +27,6 @@ CREATE TABLE `country` (
 INSERT INTO `country` (`id`, `code`, `name`, `created_at`, `updated_at`) VALUES
 (1, 'US', 'United States', '2018-04-26 17:00:00', '2018-04-26 17:00:00');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `country`
---
-ALTER TABLE `country`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `country`
---
-ALTER TABLE `country`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
